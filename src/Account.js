4ea089f825958c6fd10971c8c041954221b12f18
Simon Capriles <simon.capriles@kyohei.com.bo>
2021-09-29 22:47:22 -0400

    Project update: -Updated Readme file

--- src/Account.js
+++ src/Account.js
@@ -1,9 +1,15 @@
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 const Account = ({account, index, remove, select}) => {
+
     return (
         <div>
-            <div className="account" onClick={function(){select(account.Number)}}>{account.Number} <Button className="delete" onClick={function(){remove(index)}}>Delete</Button></div>
+            <div className="account" onClick={function(){select(account.Number, account.Balance)}}>
+                Acc: {account.Number} | Balance: {account.Balance}
+            </div>
+            <Button className="delete btn-danger" onClick={function(){remove(index)}}>
+                Delete
+            </Button>
         </div>
     )
 };
